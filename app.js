ea79fafd6260cf193fd643f1130d651da6e0ef0b
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-12 11:15:41 -0300

    array ..  looping and wrinting 90% what a programer need to know

---
+++ app.js
@@ -0,0 +1,7 @@
+let myColors = ['red', 'orange', 'yellow', 'green']
+
+myColors.forEach(saySomethingNice)
+
+function saySomethingNice(color){
+document.write("The color " + color + " is a great color.<br>")
+}
