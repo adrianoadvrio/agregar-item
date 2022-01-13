d1d079aa2d50af0e1d30bf716fc9a53a94d22182
adrianoadvrio <adrianoadvrio@yahoo.com.br>
2022-01-13 18:03:23 -0300

    new js file

--- app.js
+++
@@ -1,7 +0,0 @@
-let myColors = ['red', 'orange', 'yellow', 'green']
-
-myColors.forEach(saySomethingNice)
-
-function saySomethingNice(color){
-document.write("The color " + color + " is a great color.<br>")
-}
